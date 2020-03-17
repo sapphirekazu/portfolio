495854318c2f7fa9de8d99692d694fefe0894faf
kazumi sapphire <smilekkn@gmail.com>
2020-03-18 02:41:38 +0900

    問い合わせフォーム作成中

--- js/script.js
+++ js/script.js
@@ -1,57 +1,83 @@
 // PhotoSwipe
 initPhotoSwipeFromDOM('.my-gallery');
 
-$(function() {
+$(function () {
 
-  $('figcaption').on('click', 'a', function(e) {
-    e.stopPropagation();
-  });
+    $('figcaption').on('click', 'a', function (e) {
+        e.stopPropagation();
+    });
 
-  $('a[href^="#"]').on('click', function() {
-    var speed = 300;
-    var href = $(this).attr("href");
-    var target = $(href == "#" || href == "" ? 'html' : href);
-    var position = target.offset().top;
-    $("html, body").animate({
-      scrollTop: position
-    }, speed, "swing");
-    return false;
-  });
+    $('a[href^="#"]').on('click', function () {
+        var speed = 300;
+        var href = $(this).attr("href");
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        var position = target.offset().top;
+        $("html, body").animate({
+            scrollTop: position
+        }, speed, "swing");
+        return false;
+    });
 
-  //backToTop
-  $('#backToTop').on('click', function() {
-    $('body,html').animate({
-      scrollTop: 0
-    }, 300);
-    return false;
-  });
+    //backToTop
+    $('#backToTop').on('click', function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 300);
+        return false;
+    });
 
 
-  var $nav = $('#gnav');
-  var offset = $nav.offset();
-  var navHeight = $nav.innerHeight();
-  $('.section').css('padding-top', navHeight / 2);
-
-  $(window).on('resize', function() {
-    var currentWidth = window.innerWidth;
-    if (currentWidth == window.innerWidth) {
-      // ウインドウ横幅が変わっていないため処理をキャンセル
-      return;
-    }
-    $nav = $('#gnav');
-    offset = $nav.offset();
-    navHeight = $nav.innerHeight();
+    var $nav = $('#gnav');
+    var offset = $nav.offset();
+    var navHeight = $nav.innerHeight();
     $('.section').css('padding-top', navHeight / 2);
-  });
+
+    $(window).on('resize', function () {
+        var currentWidth = window.innerWidth;
+        if (currentWidth == window.innerWidth) {
+            //ウインドウ横幅が変わっていないため処理をキャンセル
+            return;
+        }
+        $nav = $('#gnav');
+        offset = $nav.offset();
+        navHeight = $nav.innerHeight();
+        $('.section').css('padding-top', navHeight / 2);
+    });
 
 
-  $(window).scroll(function() {
-    if ($(window).scrollTop() >= offset.top) {
-      $nav.addClass('fixed');
-      $("#contents").css("margin-top", navHeight);
-    } else {
-      $nav.removeClass('fixed');
-      $("#contents").css("margin-top", "0");
-    }
-  });
+    $(window).scroll(function () {
+        if ($(window).scrollTop() >= offset.top) {
+            $nav.addClass('fixed');
+            $("#contents").css("margin-top", navHeight);
+        } else {
+            $nav.removeClass('fixed');
+            $("#contents").css("margin-top", "0");
+        }
+    });
+});
+
+$('#submit-button').on('click', function (event) {
+    event.preventDefault();
+    //お問い合わせフォーム送信ボタン押してページ遷移させない
+    console.log("送信します");
+    $.ajax({
+        url: "https://docs.google.com/forms/u/0/d/e/1FAIpQLSdcruyiaCbQeuOy3saXbfIEdpFDTZ9BqQhht2-lJPRNF5uXUA/formResponse",
+        data: {
+            "entry.698546383": name,
+            "entry.2118870447": email,
+            "entry.1753307972": msg
+        },
+        type: "POST",
+        dataType: "xml",
+        statusCode: {
+            0: function () {
+                alert("送信完了しました");
+                //Success message
+            },
+            200: function () {
+                alert("送信に失敗しました");
+                //Success Message
+            }
+        }
+    });
 });
